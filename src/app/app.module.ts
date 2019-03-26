493ceec2c42996063292132fceec2b7ed25b026e
Orilow <ilyaorlov1998@gmail.com>
2019-03-27 00:48:18 +0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PersonInfoComponent} from './person-info/person-info.component';
+import { PaymentsComponent } from './payments/payments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonInfoComponent,
+    PaymentsComponent
   ],
   imports: [
     BrowserModule
